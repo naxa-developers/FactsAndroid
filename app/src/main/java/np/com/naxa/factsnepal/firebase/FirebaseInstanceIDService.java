9e4060b1bc9f4092ebbea653241388f3a5d5f24f
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-03-07 11:38:30 +0545

    notification-added

---
+++ app/src/main/java/np/com/naxa/factsnepal/firebase/FirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package np.com.naxa.factsnepal.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by rowsun on 5/31/16.
+ */
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
