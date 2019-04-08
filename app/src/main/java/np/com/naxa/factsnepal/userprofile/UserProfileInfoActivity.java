bdf5b14f16c099dd3b39b432cdef19c40ee203ad
samirdangal <samirdangal@gmail.com>
2019-04-08 16:51:35 +0545

    [#18] user profile info page created

---
+++ app/src/main/java/np/com/naxa/factsnepal/userprofile/UserProfileInfoActivity.java
@@ -0,0 +1,15 @@
+package np.com.naxa.factsnepal.userprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import np.com.naxa.factsnepal.R;
+
+public class UserProfileInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_info);
+    }
+}
