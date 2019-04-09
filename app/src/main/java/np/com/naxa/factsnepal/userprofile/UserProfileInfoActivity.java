29812023d294b78f502dfed219764e7706278dd6
samirdangal <samirdangal@gmail.com>
2019-04-09 11:25:42 +0545

    [#18] profile info page UI update

--- app/src/main/java/np/com/naxa/factsnepal/userprofile/UserProfileInfoActivity.java
+++ app/src/main/java/np/com/naxa/factsnepal/userprofile/UserProfileInfoActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import np.com.naxa.factsnepal.R;
+import np.com.naxa.factsnepal.common.BaseActivity;
 
-public class UserProfileInfoActivity extends AppCompatActivity {
+public class UserProfileInfoActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_info);
+
+        setupToolbar("Your Profile");
     }
 }
