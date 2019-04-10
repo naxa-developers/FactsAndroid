643f552f220182cf0d49cf6d2115adb54e9381b6
samirdangal <samirdangal@gmail.com>
2019-04-10 14:38:41 +0545

    [#33] survey company listing

--- app/src/main/java/np/com/naxa/factsnepal/surveys/SurveyCompanyListActivity.java
+++ app/src/main/java/np/com/naxa/factsnepal/surveys/SurveyCompanyListActivity.java
@@ -2,18 +2,31 @@ package np.com.naxa.factsnepal.surveys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import np.com.naxa.factsnepal.R;
 import np.com.naxa.factsnepal.common.BaseActivity;
+import np.com.naxa.factsnepal.common.BaseRecyclerViewAdapter;
+import np.com.naxa.factsnepal.userprofile.SurveyEarningHistoryVH;
 
 public class SurveyCompanyListActivity extends BaseActivity {
     private static final String TAG = "SurveyCompanyListActivity";
+    private BaseRecyclerViewAdapter<SurveyCompany, SurveyItemListVH> adapter;
+
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +48,12 @@ public class SurveyCompanyListActivity extends BaseActivity {
         }
     }
 
+    List<SurveyCompany> surveyCompanies;
     private void getSurveyWuestionDetailsResponse(){
         final String[] jsonInString = {""};
         Gson gson = new Gson();
+
+        surveyCompanies = new ArrayList<SurveyCompany>();
 
         apiInterface.getSurveyQuestionDetailsResponse()
                 .subscribeOn(Schedulers.io())
@@ -45,8 +61,10 @@ public class SurveyCompanyListActivity extends BaseActivity {
                 .subscribe(new DisposableObserver<SurveyQuestionDetails>() {
                     @Override
                     public void onNext(SurveyQuestionDetails surveyQuestionDetails) {
-
                         jsonInString[0] = gson.toJson(surveyQuestionDetails);
+                        surveyCompanies = surveyQuestionDetails.getSurveyCompany();
+                        Log.d(TAG, "onComplete: company list size "+surveyQuestionDetails.getSurveyCompany().size());
+
                     }
 
                     @Override
@@ -57,8 +75,43 @@ public class SurveyCompanyListActivity extends BaseActivity {
                     @Override
                     public void onComplete() {
                         hideProgressDialog();
+                        setupRecyclerView(surveyCompanies);
                         Log.d(TAG, "onComplete: "+jsonInString[0]);
+                        Log.d(TAG, "onComplete: company list size "+surveyCompanies.size());
                     }
                 });
     }
+
+
+    private void setupRecyclerView (List<SurveyCompany> surveyCompanyList){
+        recyclerView = findViewById(R.id.rv_survey_company_list);
+
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new BaseRecyclerViewAdapter<SurveyCompany, SurveyItemListVH>(surveyCompanyList, R.layout.survey_list_item_row_layout) {
+
+
+            @Override
+            public void viewBinded(SurveyItemListVH surveyItemListVH, SurveyCompany surveyCompany, int position) {
+                surveyItemListVH.bindView(surveyCompany);
+                surveyItemListVH.itemView.setOnClickListener((v -> {
+
+                }));
+            }
+
+            @Override
+            public SurveyItemListVH attachViewHolder(View view) {
+                return new SurveyItemListVH(view);
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/np/com/naxa/factsnepal/surveys/SurveyItemListVH.java
@@ -0,0 +1,33 @@
+package np.com.naxa.factsnepal.surveys;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import np.com.naxa.factsnepal.FactsNepal;
+import np.com.naxa.factsnepal.R;
+import np.com.naxa.factsnepal.utils.ImageUtils;
+
+public class SurveyItemListVH extends RecyclerView.ViewHolder {
+    private TextView tvSurveyName, tvSurveyDate;
+    ImageView ivSurveyImage;
+
+
+    public SurveyItemListVH(@NonNull View itemView) {
+        super(itemView);
+        tvSurveyName = itemView.findViewById(R.id.tv_survey_list_item_title);
+        tvSurveyDate = itemView.findViewById(R.id.tv_survey_list_item_date);
+        ivSurveyImage = itemView.findViewById(R.id.iv_survey_list_item_image);
+
+    }
+
+    void bindView(SurveyCompany surveyCompany) {
+        tvSurveyName.setText(surveyCompany.getTitle());
+        tvSurveyDate.setText(surveyCompany.getShortdesc());
+
+        ImageUtils.loadRemoteImage(FactsNepal.getInstance(), surveyCompany.getImage());
+
+    }
+}
