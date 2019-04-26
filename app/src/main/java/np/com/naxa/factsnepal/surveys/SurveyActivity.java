fb531fd9751918e1744c61079d3f80d462584018
Nishon Tandukar <nishon.tan@gmail.com>
2019-04-26 14:17:20 +0545

    [#103] refactor support lib layout to androidx

--- app/src/main/java/np/com/naxa/factsnepal/surveys/SurveyActivity.java
+++ app/src/main/java/np/com/naxa/factsnepal/surveys/SurveyActivity.java
@@ -2,19 +2,20 @@ package np.com.naxa.factsnepal.surveys;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import androidx.annotation.Nullable;
-
 
 import np.com.naxa.factsnepal.R;
 import np.com.naxa.factsnepal.common.BaseActivity;
