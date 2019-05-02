c37a6025a2216b7b83fb7c72719eff87151a5bfb
samirdangal <samirdangal@gmail.com>
2019-05-02 15:24:04 +0545

    [#149] un bookmark alert dialog added

--- app/src/main/java/np/com/naxa/factsnepal/bookmarkedfeed/BookmarkedFeedV2Activity.java
+++ app/src/main/java/np/com/naxa/factsnepal/bookmarkedfeed/BookmarkedFeedV2Activity.java
@@ -1,10 +1,14 @@
 package np.com.naxa.factsnepal.bookmarkedfeed;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -13,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import io.reactivex.schedulers.Schedulers;
 import np.com.naxa.factsnepal.R;
 import np.com.naxa.factsnepal.common.BaseActivity;
 import np.com.naxa.factsnepal.common.BaseRecyclerViewAdapter;
@@ -20,6 +25,7 @@ import np.com.naxa.factsnepal.common.Constant;
 import np.com.naxa.factsnepal.feed.Fact;
 import np.com.naxa.factsnepal.feed.FactsLocalSource;
 import np.com.naxa.factsnepal.feed.detail.FactDetailActivity;
+import np.com.naxa.factsnepal.utils.DialogUtils;
 
 public class BookmarkedFeedV2Activity extends BaseActivity {
 
@@ -64,6 +70,7 @@ public class BookmarkedFeedV2Activity extends BaseActivity {
                     public void unBookmarkedListner(Fact fact) {
                         Log.d("Bookmarked", "unBookmarkedListner: ");
 
+                       createUnBookmarkAlertDialog(fact);
                     }
 
                     @Override
@@ -92,6 +99,30 @@ public class BookmarkedFeedV2Activity extends BaseActivity {
         ActivityOptionsCompat options = ActivityOptionsCompat.
                 makeSceneTransitionAnimation(this, imageView, getString(R.string.transtion_fact_list_details));
         startActivity(intent, options.toBundle());
+    }
+
+
+    private void createUnBookmarkAlertDialog (Fact fact){
+        new AlertDialog.Builder(BookmarkedFeedV2Activity.this)
+                .setTitle("Un Bookmark Fact")
+                .setMessage("Are you sure you want to Un Bookmark this Fact?")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                        FactsLocalSource.getINSTANCE().toggleBookMark(fact)
+                                .subscribeOn(Schedulers.io())
+                                .subscribe();
+                        Toast.makeText(BookmarkedFeedV2Activity.this, "Un Bookmarked Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
 }
